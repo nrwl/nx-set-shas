179f3c4d726d932467c28e650f2383dc102943c2
Miroslav Jonas <missing.manual@gmail.com>
2021-07-21 10:57:12 +0200

    feat: convert entire action logic to node script

--- find-successful-workflow.js
+++ find-successful-workflow.js
@@ -1,44 +1,92 @@
 const { Octokit } = require("@octokit/action");
 const core = require("@actions/core");
+const github = require('@actions/github');
 const { execSync } = require('child_process');
 
-const token = process.argv[2];
-const branch = process.argv[3];
-const workflowId = process.argv[4];
-const [owner, repo] = process.env.GITHUB_REPOSITORY.split("/");
-const run_id = process.env.GITHUB_RUN_ID;
-process.env.GITHUB_TOKEN = token;
+const { runId, repo: { repo, owner }, eventName } = github.context;
+process.env.GITHUB_TOKEN = process.argv[2];
+const mainBranchName = process.argv[3];
+const errorOnNoSuccessfulWorkflow = process.argv[4];
+const workflowId = process.argv[5];
 
+let BASE_SHA;
 (async () => {
-  try {
-    const octokit = new Octokit();
-    let workflow_id = workflowId;
-    if (!workflow_id) {
-      // retrieve workflow-id
-      workflow_id = await octokit.request(`GET /repos/${owner}/${repo}/actions/runs/${run_id}`, {
-        owner,
-        repo,
-        branch,
-        run_id
-      }).then(({ data: { workflow_id } }) => workflow_id);
+  const HEAD_SHA = execSync(`git rev-parse HEAD`, { encoding: 'utf-8' });
+
+  if (eventName === 'pull_request') {
+    BASE_SHA = execSync(`git merge-base origin/${mainBranchName} HEAD`, { encoding: 'utf-8' });
+  } else {
+    try {
+      BASE_SHA = await findSuccessfulCommit(workflowId, runId, owner, repo, mainBranchName);
+    } catch (e) {
+      core.setFailed(e.message);
+      return;
     }
-    // fetch all workflow runs on a given repo/branch/workflow with push and success
-    const shas = await octokit.request(`GET /repos/${owner}/${repo}/actions/workflows/${workflow_id}/runs`, {
+
+    if (!BASE_SHA) {
+      if (errorOnNoSuccessfulWorkflow === 'true') {
+        reportFailure(mainBranchName);
+        return;
+      } else {
+        process.stdout.write('\n');
+        process.stdout.write(`WARNING: Unable to find a successful workflow run on 'origin/${mainBranchName}'\n`);
+        process.stdout.write(`We are therefore defaulting to use HEAD~1 on 'origin/${mainBranchName}'\n`);
+        process.stdout.write('\n');
+        process.stdout.write(`NOTE: You can instead make this a hard error by settting 'error-on-no-successful-workflow' on the action in your workflow.\n`);
+
+        BASE_SHA = execSync(`git rev-parse HEAD~1`, { encoding: 'utf-8' });
+      }
+    } else {
+      process.stdout.write('\n');
+      process.stdout.write(`Found the last successful workflow run on 'origin/${mainBranchName}'\n`);
+      process.stdout.write(`Commit: ${BASE_SHA}\n`);
+    }
+  }
+  core.setOutput('base', BASE_SHA);
+  core.setOutput('head', HEAD_SHA);
+})();
+
+function reportFailure(branchName) {
+  core.setFailed(`
+    Unable to find a successful workflow run on 'origin/${branchName}'
+    NOTE: You have set 'error-on-no-successful-workflow' on the action so this is a hard error.
+
+    Is it possible that you have no runs currently on 'origin/${branchName}'?
+    - If yes, then you should run the workflow without this flag first.
+    - If no, then you might have changed your git history and those commits no longer exist.`);
+}
+
+/**
+ * Find last successful workflow run on the repo
+ * @param {string?} workflow_id
+ * @param {number} run_id
+ * @param {string} owner
+ * @param {string} repo
+ * @param {string} branch
+ * @returns
+ */
+async function findSuccessfulCommit(workflow_id, run_id, owner, repo, branch) {
+  const octokit = new Octokit();
+  if (!workflow_id) {
+    workflow_id = await octokit.request(`GET /repos/${owner}/${repo}/actions/runs/${run_id}`, {
       owner,
       repo,
       branch,
-      workflow_id,
-      event: 'push',
-      status: 'success'
-    }).then(({ data: { workflow_runs } }) => workflow_runs.map(run => run.head_sha));
-
-    const sha = await findExistingCommit(shas);
-    console.log(sha);
-  } catch (e) {
-    core.setFailed(e.message);
-    process.exit(1);
+      run_id
+    }).then(({ data: { workflow_id } }) => workflow_id);
   }
-})();
+  // fetch all workflow runs on a given repo/branch/workflow with push and success
+  const shas = await octokit.request(`GET /repos/${owner}/${repo}/actions/workflows/${workflow_id}/runs`, {
+    owner,
+    repo,
+    branch,
+    workflow_id,
+    event: 'push',
+    status: 'success'
+  }).then(({ data: { workflow_runs } }) => workflow_runs.map(run => run.head_sha));
+
+  return await findExistingCommit(shas);
+}
 
 /**
  * Get first existing commit
