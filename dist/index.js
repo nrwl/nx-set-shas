04afd290c474773f3b463cd6ff493857207b0254
Wazbat <wazbat97@gmail.com>
2022-01-31 13:08:59 +0100

    Added output to indicate when no previous build was found on this branch (#16)
    * Added output to indicate when no previous build
    
    * Incremented version
    
    * Build updated code
    
    * Update package-lock.json
    
    Co-authored-by: Miroslav Jonaš <meeroslav@users.noreply.github.com>

--- dist/index.js
+++ dist/index.js
@@ -6400,6 +6400,7 @@ let BASE_SHA;
         process.stdout.write(`NOTE: You can instead make this a hard error by settting 'error-on-no-successful-workflow' on the action in your workflow.\n`);
 
         BASE_SHA = execSync(`git rev-parse HEAD~1`, { encoding: 'utf-8' });
+        core.setOutput('noPreviousBuild', 'true');
       }
     } else {
       process.stdout.write('\n');
