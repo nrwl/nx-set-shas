5db8718a113d67ca2f53663d40c0833fea93c142
Miroslav Jonas <missing.manual@gmail.com>
2022-03-16 11:19:26 +0100

    chore: add information on existing workflow

--- dist/index.js
+++ dist/index.js
@@ -6440,6 +6440,8 @@ async function findSuccessfulCommit(workflow_id, run_id, owner, repo, branch, la
       branch,
       run_id
     }).then(({ data: { workflow_id } }) => workflow_id);
+    process.stdout.write('\n');
+    process.stdout.write(`Workflow Id not provided. Using workflow '${workflow_id}'\n`);
   }
   // fetch all workflow runs on a given repo/branch/workflow with push and success
   const shas = await octokit.request(`GET /repos/${owner}/${repo}/actions/workflows/${workflow_id}/runs`, {
