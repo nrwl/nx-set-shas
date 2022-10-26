18b5eb403e534cbabbe52e16d0a961837ad58ff3
Miroslav Jonas <missing.manual@gmail.com>
2022-10-26 16:22:39 +0200

    chore: update ncc

--- dist/setup-sandbox.js
+++ dist/setup-sandbox.js
@@ -1,14 +1,6 @@
-/******/ (() => { // webpackBootstrap
-/******/ 	"use strict";
-/******/ 	/* webpack/runtime/compat */
-/******/ 	
-/******/ 	if (typeof __nccwpck_require__ !== 'undefined') __nccwpck_require__.ab = __dirname + "/";
-/******/ 	
-/************************************************************************/
-var __webpack_exports__ = {};
 /* global host, bridge, data, context */
 
-
+'use strict';
 
 const {
 	Object: localObject,
@@ -459,7 +451,3 @@ return {
 	readonly,
 	global
 };
-
-module.exports = __webpack_exports__;
-/******/ })()
-;
